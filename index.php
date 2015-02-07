ff9c63f5f49dbae65da2c9e655e6bf1de34178d4
Chris Tyrrel <me@christyrrel.me>
2015-02-07 16:05:35 -0700

    Changed capitilization of IPv6

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <h3>FususIRC</h3>
             <p>FususIRC is an IRC network created with the purpose of introducing teens and young adults into computing whilst providing an environment that shows no judgement towards any user.</p>
             <h3>Connections</h3>
-            <p>FususIRC servers accept connections via the round robin <b>irc.fusus.org</b>. IPv6-only connections may utilize <b>IPv6.fusus.org</b> to connect.</p>
+            <p>FususIRC servers accept connections via the round robin <b>irc.fusus.org</b>. IPv6-only connections may utilize <b>ipv6.fusus.org</b> to connect.</p>
             <p>Plaintext connections are accepted globally using ports <b>6667-6669</b>. SSL connections are accepted on port <b>+6697</b> globally.</p>
         </div>
 <?php include('resources/components/footer.php'); ?>
