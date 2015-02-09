e9079751c243302650df2afe7466b28694cb826e
Chris Tyrrel <me@christyrrel.me>
2015-02-08 20:36:07 -0700

    Reorganize SSL certificate fingerprints

--- servers.php
+++ servers.php
@@ -12,7 +12,6 @@
                         <th>Location</th>
                         <th>Administration</th>
                         <th>Operators</th>
-                        <th>SHA1 Fingerprint</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -21,21 +20,18 @@
                         <td>New York, New York</td>
                         <td>Jordan</td>
                         <td>None</td>
-                        <td>80:4B:65:D9:68:0B:A1:EB:28:9E:E8:D3:04:57:AE:ED:90:81:84:74</td>
                     </tr>
                     <tr class="info">
                         <td>Griffith</td>
                         <td>Los Angeles, California</td>
                         <td>ChasedSpade</td>
                         <td>None</td>
-                        <td>F4:C9:9E:DE:F3:CE:3F:64:9C:08:56:6A:40:83:EB:95:1D:5B:BA:61</td>
                     </tr>
                     <tr class="info">
                         <td>Maple</td>
                         <td>Beauharnois, Quebec</td>
                         <td>Jordan</td>
                         <td>None</td>
-                        <td>13:79:D6:36:D1:51:14:AE:0A:31:8D:83:82:47:D2:D2:D3:A9:E6:C9</td>
                     </tr>
                 </tbody>
             </table>
@@ -55,7 +51,33 @@
                         <td>Roubaix, France</td>
                         <td>Yoda</td>
                         <td>None</td>
+                    </tr>
+                </tbody>
+            </table>
+            <h5>SSL Fingerprints</h5>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Server Name</th>
+                        <th>SHA1 Fingerprint</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="info">
+                        <td>Empire</td>
+                        <td>80:4B:65:D9:68:0B:A1:EB:28:9E:E8:D3:04:57:AE:ED:90:81:84:74</td>
+                    </tr>
+                    <tr class="info">
+                        <td>Griffith</td>
+                        <td>F4:C9:9E:DE:F3:CE:3F:64:9C:08:56:6A:40:83:EB:95:1D:5B:BA:61</td>
+                    </tr>
+                    <tr class="info">
+                        <td>Louvre</td>
                         <td>49:A4:B9:50:32:CF:D0:27:65:8C:E4:73:2E:05:CF:1B:7C:37:6A:99</td>
+                    </tr>
+                    <tr class="info">
+                        <td>Maple</td>
+                        <td>13:79:D6:36:D1:51:14:AE:0A:31:8D:83:82:47:D2:D2:D3:A9:E6:C9</td>
                     </tr>
                 </tbody>
             </table>
