3e41efa8f8bc40af41088332ad9bdcaa8d67d6d3
Chris Tyrrel <me@christyrrel.me>
2015-05-11 22:16:13 -0600

    SSL Fingerprints are inaccurate/not nessecary.

--- servers.php
+++ servers.php
@@ -54,33 +54,5 @@
                     </tr>
                 </tbody>
             </table>
-            <h4 class="text-center">SSL Fingerprints</h4>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Server Name</th>
-                        <th>SHA1 Fingerprint</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="info">
-                        <td>Empire</td>
-                        <td>80:4B:65:D9:68:0B:A1:EB:28:9E:E8:D3:04:57:AE:ED:90:81:84:74</td>
-                    </tr>
-                    <tr class="info">
-                        <td>Griffith</td>
-                        <td>F4:C9:9E:DE:F3:CE:3F:64:9C:08:56:6A:40:83:EB:95:1D:5B:BA:61</td>
-                    </tr>
-                    <tr class="info">
-                        <td>Louvre</td>
-                        <td>49:A4:B9:50:32:CF:D0:27:65:8C:E4:73:2E:05:CF:1B:7C:37:6A:99</td>
-                    </tr>
-                    <tr class="info">
-                        <td>Maple</td>
-                        <td>13:79:D6:36:D1:51:14:AE:0A:31:8D:83:82:47:D2:D2:D3:A9:E6:C9</td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
 <?php include('resources/components/footer.php'); ?>
-            
